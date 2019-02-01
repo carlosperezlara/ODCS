ce85d5ef4c71c81e26451d9d2f51d18a5c137bd0
carlosperezlara <cperez@Perezs-MBP.home>
2019-02-01 01:30:21 -0500

    cleanup

--- MPGD4EIC/MF_Position/Run_MF_Position.C
+++ MPGD4EIC/MF_Position/Run_MF_Position.C
@@ -1,11 +1,12 @@
 #include <TGClient.h>
 #include "MF_Position.h"
+#include "MF_Position.cxx"
 
 //====================
 //====================
 //====================
 //====================
-int main() {
+int Run_MF_Position() {
   // Popup the GUI...
   new MF_Position(gClient->GetRoot(), 900, 800);
   return 0;
